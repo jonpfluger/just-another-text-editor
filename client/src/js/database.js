7628bb80a6f0f0ec018a0e3a0d977dbee50ac08a
jonpfluger <jonpfluger718@gmail.com>
2023-01-18 11:22:56 -0600

    database.js, install.js, gitignore

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jate = await openDB('jate', 1)
+  const get = jate.transaction('jate', 'readwrite')
+  const database = get.objectStore('jate')
+  const request = database.put({id: 1, value: content})
+  const result = await request
+  return result.value
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jate = await openDB('jate', 1)
+  const get = jate.transaction('jate', 'readonly')
+  const database = get.objectStore('jate')
+  const request = database.getAll()
+  const result = await request
+  return result.value
+};
 
 initdb();
