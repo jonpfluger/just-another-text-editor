ccdb4ef59cda3ea07b107998aefb24a91078f00a
jonpfluger <jonpfluger718@gmail.com>
2023-01-18 11:53:03 -0600

    ready for heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,24 +12,22 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jate = await openDB('jate', 1)
   const get = jate.transaction('jate', 'readwrite')
   const database = get.objectStore('jate')
   const request = database.put({id: 1, value: content})
   const result = await request
-  return result.value
+  console.log(result)
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jate = await openDB('jate', 1)
   const get = jate.transaction('jate', 'readonly')
   const database = get.objectStore('jate')
   const request = database.getAll()
   const result = await request
-  return result.value
+  return result
 };
 
 initdb();
